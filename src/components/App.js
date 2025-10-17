a6a5b14fc8773b166cc7ebf409ad7c81ade50852
Abhiram302 <abhiramburra3@gmail.com>
2025-10-17 10:26:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 
 import React from "react";
-import './../styles/App.css';
-import PageLayout from "./PageLayout.js"
+import "./../styles/App.css";
+import PageLayout from "./PageLayout.js";
 
 const App = () => {
-  let head = "Welcome to my website"
-  let foot = "© 2023 My Website. All rights reserved"
-  let child = "This is the content of my website"
+  let head = <h1>Welcome to my website</h1>;
+  let foot = <footer>© 2023 My Website. All rights reserved</footer>;
+  let child = <p>This is the content of my website</p>;
+
   return (
     <div>
-        {/* Do not remove the main div */}
-      <PageLayout header = {head} footer = {foot} children = {child}/>
+      {/* Do not remove the main div */}
+      <PageLayout header={head} footer={foot}>
+        {child}
+      </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

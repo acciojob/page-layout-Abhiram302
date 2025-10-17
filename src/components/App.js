e58c546dcb649b730ad626936af3a1f0af20d543
Abhiram302 <abhiramburra3@gmail.com>
2025-10-17 10:16:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let head = "Welcome to my website"
+  let foot = "© 2023 My Website. All rights reserved"
+  let child = "This is the content of my website"
   return (
     <div>
         {/* Do not remove the main div */}
+      <PageLayout header = {head} footer = {foot} children = {child}/>
     </div>
   )
 }
